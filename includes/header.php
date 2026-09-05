cca2bc61963cb2d202f06c4f8c5cc86e8ce88ee1
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-09-05 20:51:12 +0700

    commit header

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Project_KHAS - Manajemen Data Khas Karang Taruna</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="bg-gray-50 text-gray-800 font-sans min-h-screen flex flex-col justify-between">
